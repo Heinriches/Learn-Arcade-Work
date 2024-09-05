6b0a524192d2ac3f53e4320bc2f6b743b9c316cb
Fabio <heydrich.institut@gmail.com>
2024-09-05 16:25:53 -0300

    4.8.1. Lab 1: First Program

--- Testing/test.py
+++ Testing/test.py
@@ -1,1 +1,2 @@
-print("Hello World!")
+print("Hi! This is a double-quote mark: \" ; we use it for printing.")
+print("Aubrey Hepburn once said: \"Nothing is impossible. The word itself says 'I'm impossible.'\"")
